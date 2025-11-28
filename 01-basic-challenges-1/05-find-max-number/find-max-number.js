22f1bfca6d2f542fc37886d7cdc0c228a5fcfec4
NancyAsiuloka <nancyasiuloka@gmail.com>
2025-11-28 17:50:37 +0100

    Implement findMaxNumber function with manual max calculation

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,5 +1,15 @@
-function findMaxNumber(arr) {
-    return Math.max(...arr)
+// function findMaxNumber(arr) {
+//     return Math.max(...arr)
+// }
+
+function findMaxNumber(arr){
+    let max = arr[0];
+    for(let i = 1; i < arr.length; i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max
 }
 
 module.exports = findMaxNumber;
