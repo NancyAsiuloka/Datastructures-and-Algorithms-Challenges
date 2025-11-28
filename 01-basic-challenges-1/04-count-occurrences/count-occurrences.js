d2db9f33395116ec5d879f65ede04f65ac63f5e5
NancyAsiuloka <nancyasiuloka@gmail.com>
2025-11-28 17:43:19 +0100

    Add documentation for countOccurrences function

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -8,6 +8,13 @@
 //     return count;
 // }
 
+/**
+ * The function `countOccurrences` takes a string and a character as input, then returns the number of
+ * occurrences of that character in the string.
+ * @param str - A string that you want to search for occurrences of a specific character.
+ * @param char - The `char` parameter in the `countOccurrences` function represents the character that
+ * you want to count the occurrences of in the `str` parameter.
+ */
 const countOccurrences = (str, char) => str.split(char).length - 1
 
 
