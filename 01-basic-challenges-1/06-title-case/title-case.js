2c924c244fe91f3f5c5cb60d512f3fe2366213a4
NancyAsiuloka <nancyasiuloka@gmail.com>
2025-11-28 18:06:33 +0100

    Refactor titleCase function to use a for loop for word capitalization

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,6 +1,15 @@
-function titleCase(str) {
-    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
+// function titleCase(str) {
+//     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
 
+// }
+
+function titleCase(str) {
+    const words = str.toLowerCase().split(' ');
+    for (let i =0; i < words.length; i++){
+        words[i]= words[i][0].toUpperCase() + words[i].slice(1)
+    }
+
+    return words.join(' ')
 }
 
 module.exports = titleCase;
