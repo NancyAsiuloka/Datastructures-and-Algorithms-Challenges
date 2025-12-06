1b90f330fc61636cd47d7f92fc61d4a4a2db508c
NancyAsiuloka <nancyasiuloka@gmail.com>
2025-12-06 18:09:37 +0100

    Added a comment

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,7 +1,7 @@
 function isPalindrome(str) {
     const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    const reversedStr = formattedStr.split('').reversed().join('')
-    return formattedStr;
+    const reversedStr = formattedStr.split('').reverse().join('');
+    return formattedStr  === reversedStr;
 }
 
 module.exports = isPalindrome;
