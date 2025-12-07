1b02fe53f0bbc8c5016123ec1bdb56535b524d45
NancyAsiuloka <nancyasiuloka@gmail.com>
2025-12-07 18:06:35 +0100

    Added a fucntions to check a palindrome string

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,7 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    return formattedStr;
+}
 
 module.exports = isPalindrome;
