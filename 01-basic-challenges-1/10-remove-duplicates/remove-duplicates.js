cd6313fcb5e72ae1247bfeed612529063692301f
NancyAsiuloka <nancyasiuloka@gmail.com>
2025-12-08 18:58:59 +0100

    Added a function to rmove duplicates in an array using Set

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,13 +1,18 @@
+// function removeDuplicates(arr) {
+//     const uniqueArray = [];
+
+//     for(let i = 0; i < arr.length; i++){
+//         if(!uniqueArray.includes(arr[i])){
+//             uniqueArray.push(arr[i]);
+//         }
+//     }
+
+//     return uniqueArray;
+// }
+
 function removeDuplicates(arr) {
-    const uniqueArray = [];
-
-    for(let i = 0; i < arr.length; i++){
-        if(!uniqueArray.includes(arr[i])){
-            uniqueArray.push(arr[i]);
-        }
-    }
-
-    return uniqueArray;
+    console.log(arr)
+    return Array.from(new Set(arr));
 }
 
 module.exports = removeDuplicates;
