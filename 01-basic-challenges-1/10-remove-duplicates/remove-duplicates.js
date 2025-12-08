2fdc4ce52a4f64bdbd7499b7e6b1a13ffc65da7e
NancyAsiuloka <nancyasiuloka@gmail.com>
2025-12-08 18:54:29 +0100

    Added a function to rmove duplicates in an array using for loop

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,13 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+    const uniqueArray = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if(!uniqueArray.includes(arr[i])){
+            uniqueArray.push(arr[i]);
+        }
+    }
+
+    return uniqueArray;
+}
 
 module.exports = removeDuplicates;
